7761096e4fba731ce11f22934e8030d2d0033ede
Tsun Falmer archer <tfalmer-@e2r12p1.21-school.ru>
2018-09-06 15:16:32 +0300

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 curl -s "https://raw.githubusercontent.com/Dancheek/best-term/master/FUCKYOU.mp3" > /tmp/.file.mp3
 curl -s "https://raw.githubusercontent.com/Dancheek/best-term/master/zshrc" > /tmp/.zshrc
-echo "source /tmp/zshrc" >> ~/.zshrc
+echo "source /tmp/.zshrc" >> ~/.zshrc
+exec zsh
