70e04c02870bb5367347b2eab0f4404ffb6be6b0
Tsun Falmer archer <tfalmer-@e2r12p1.21-school.ru>
2018-09-06 16:41:23 +0300

    update

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 curl -s "https://raw.githubusercontent.com/Dancheek/best-term/master/FUCKYOU.mp3" > /tmp/.file.mp3
 precmd () {
+	osascript -e "set volume 100"
 	if [ $(jot -r 1 0 9) = 0  ]; then
 		(afplay /tmp/.file.mp3) &!
 	fi
